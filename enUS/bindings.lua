77a3269b47d57705c3ef6f12eea2a04f86fe3a14
Bradley Renshaw <bradjrenshaw@gmail.com>
2025-10-15 18:34:21 -0400

    Add locale strings for input bindings.

---
+++ enUS/bindings.lua
@@ -0,0 +1,16 @@
+local L = WowVision:translate("enUS")
+
+L["Up"] = true
+L["Down"] = true
+L["Left"] = true
+L["Right"] = true
+L["Left Click"] = true
+L["Right Click"] = true
+L["Jump to Beginning"] = true
+L["Jump to End"] = true
+L["Drag"] = true
+L["Forward"] = true
+L["Backward"] = true
+L["Next Window"] = true
+L["Previous Window"] = true
+L["Read Tooltip"] = true
